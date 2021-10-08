0284ae229b7b45dcc403d1dabe4f6072f720da1b
Kyubin Lee <kyubinlee@Kyubinui-MacBookPro.local>
2021-10-08 17:29:55 +0900

    first commit

---
+++ q3-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+    int num1, num2, num3
+
+    /*printf("two int insert: ");
+    scanf("%d %d", &num1, &num2);
+    printf("minus : %d, multi : %d\n", num1-num2, num1*num2);*/
+
+    /*printf("three int insert: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    printf("result : %d\n", (num1*num2)+num3);*/
+
+    /*printf("int insert: ");
+    scanf("%d", &num1);
+    printf("result : %d\n", num1*num1);*/
+
+    printf("two int insert: ");
+    scanf("%d %d", &num1, &num2);
+    printf("quo : %d, rem : %d\n", num1 / num2, num1 % num2);
+
+    return 0;
+
+
+}
